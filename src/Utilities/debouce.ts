2a06ec1f251d4fc73274cfb644dad337544bf04e
Connor Gamble <cjgamble21@gmail.com>
2024-10-17 19:02:29 -0500

    Whoops...was my debounce method even working? :/

--- src/Utilities/debouce.ts
+++ src/Utilities/debouce.ts
@@ -5,7 +5,7 @@ export const debounce = <T, R>(func: (...args: T[]) => R, timeout = 500) => {
         new Promise<Awaited<R>>((resolve) => {
             clearTimeout(timer)
 
-            setTimeout(() => {
+            timer = setTimeout(() => {
                 const output = func(...args)
                 output instanceof Promise ? output.then(resolve) : output
             }, timeout)
